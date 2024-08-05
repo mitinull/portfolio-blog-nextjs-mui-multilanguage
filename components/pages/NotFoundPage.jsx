f9f284992977a1147b608073ba90d2af9a5720b5
Mitinul <torabi9030@gmail.com>
2024-08-05 13:22:17 +0330

    Fixing english 404 and changing 404 style

---
+++ components/pages/NotFoundPage.jsx
@@ -0,0 +1,32 @@
+import { ErrorOutline } from "@mui/icons-material";
+import { Box, Button, Stack, Typography } from "@mui/material";
+import Link from "next/link";
+
+export function NotFoundPage({ lang }) {
+  const isFa = lang === "fa";
+
+  return (
+    <Box display="flex" justifyContent="center" mt="20vh">
+      <Stack gap={2} alignItems="center">
+        <ErrorOutline sx={{ fontSize: 75, m: 2 }} color="error" />
+        <Typography variant="h4" fontWeight={450} color="error">
+          {isFa ? "ارور شمارهٔ ۴۰۴" : "Not Found! (404)"}
+        </Typography>
+        <Typography variant="h6" fontWeight={400} color="error">
+          {isFa
+            ? "اصلا همچین صفحه‌ای نداریم!"
+            : "Looks like this page is on vacation!"}
+        </Typography>
+        <Button
+          href="/fa"
+          LinkComponent={Link}
+          variant="contained"
+          sx={{ mt: 3, fontFamily: "inherit", gap: 1 }}
+          color="error"
+        >
+          {isFa ? "بازگشت به صفحهٔ اصلی" : "Go Back to Homepage"}
+        </Button>
+      </Stack>
+    </Box>
+  );
+}
