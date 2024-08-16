e24eb87df3715a78c2e5ced503a46e37a5845bae
Mitinul <torabi9030@gmail.com>
2024-08-16 22:29:33 +0330

    cache: 'no-store' for ema-english api

--- app/api/ema-english-info/route.js
+++ app/api/ema-english-info/route.js
@@ -3,7 +3,8 @@ import parse from "node-html-parser";
 export async function GET() {
   try {
     const res = await fetch(
-      "https://castbox.fm/channel/EMA-English-%7C-English-Music-%7C-%D8%A2%D9%85%D9%88%D8%B2%D8%B4-%D8%B2%D8%A8%D8%A7%D9%86-%D8%A7%D9%86%DA%AF%D9%84%DB%8C%D8%B3%DB%8C-%D9%87%D9%85%D8%B1%D8%A7%D9%87-%D8%A8%D8%A7-%D8%AA%D8%B1%D8%A7%D9%86%D9%87-id5058728?country=en"
+      "https://castbox.fm/channel/EMA-English-%7C-English-Music-%7C-%D8%A2%D9%85%D9%88%D8%B2%D8%B4-%D8%B2%D8%A8%D8%A7%D9%86-%D8%A7%D9%86%DA%AF%D9%84%DB%8C%D8%B3%DB%8C-%D9%87%D9%85%D8%B1%D8%A7%D9%87-%D8%A8%D8%A7-%D8%AA%D8%B1%D8%A7%D9%86%D9%87-id5058728?country=en",
+      { cache: "no-store" }
     );
 
     const data = await res.text();
