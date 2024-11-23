d163741e97ba034aaf6fa2df6f6560be3e596089
Mitinul <torabi9030@gmail.com>
2024-11-23 18:59:41 +0330

    Updating metadata fa and en for DiceyCrown

--- app/(index-en)/(pages)/dicey-crown/page.jsx
+++ app/(index-en)/(pages)/dicey-crown/page.jsx
@@ -1,24 +1,23 @@
-import { BasketnullPage } from "@/components/pages/basketnull/BasketnullPage";
 import { DiceyCrownPage } from "@/components/pages/dicey-crown/DiceyCrownPage";
 import { ROOT_URL } from "@/utils/constants";
 import { generateHreflangAndOg } from "@/utils/generateHreflangAndOg";
 
-const title = "Basketnull - Open-Source Lua Game";
+const title = "Dicey Crown Game";
 
 export const metadata = {
   title: title,
   description:
-    "Explore Basketnull, an open-source game by Mitinull built with Lua and Love2D. Play online and contribute new levels on GitHub.",
+    "We developed this game in one week for Amirkabir University Gamecraft 2024. Dicey Crown ranked first out of 29 games.",
 
   ...generateHreflangAndOg(
-    "basketnull",
+    "dicey-crown",
     "en",
     undefined,
-    ROOT_URL + "/images/basketnull-dark.jpg",
+    ROOT_URL + "/images/dicey-crown/dicey-crown-dark.jpg",
     title
   ),
 };
 
-export default function Basketnull() {
+export default function DiceyCrown() {
   return <DiceyCrownPage lang="en" />;
 }
--- app/fa/(pages)/dicey-crown/page.jsx
+++ app/fa/(pages)/dicey-crown/page.jsx
@@ -1,24 +1,22 @@
-import { BasketnullPage } from "@/components/pages/basketnull/BasketnullPage";
 import { DiceyCrownPage } from "@/components/pages/dicey-crown/DiceyCrownPage";
 import { ROOT_URL } from "@/utils/constants";
 import { generateHreflangAndOg } from "@/utils/generateHreflangAndOg";
 
-const title = "بسکتنال - بازی متن‌باز";
+const title = "بازی تاج پرخطر (Dicey Crown)";
 
 export const metadata = {
   title: title,
-  description:
-    "بسکتنال، یک بازی متن‌باز توسط میتینال، ساخته شده با Lua و Love2D. آنلاین بازی کنید و مراحل جدید را در گیت‌هاب اضافه کنید.",
+  description: `این بازی رو برای مسابقه بازی‌سازی دانشگاه امیرکبیر ساختیم و موفق به کسب مقام اول شدیم`,
 
   ...generateHreflangAndOg(
     "dicey-crown",
     "fa",
     undefined,
-    ROOT_URL + "/images/dicey-crown-light.jpg",
+    ROOT_URL + "/images/dicey-crown/dicey-crown-light.jpg",
     title
   ),
 };
 
-export default function Basketnull() {
+export default function DiceyCrown() {
   return <DiceyCrownPage lang="fa" />;
 }
