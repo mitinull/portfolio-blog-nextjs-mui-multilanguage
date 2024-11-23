b002b7639c9e451bfe7f6640d1b48989dfb3ae52
Mitinul <torabi9030@gmail.com>
2024-11-23 18:06:09 +0330

    Updating Heading2 lineHeight

--- components/mui-components/Heading2.jsx
+++ components/mui-components/Heading2.jsx
@@ -2,7 +2,7 @@ import { Alert, Paper, Typography } from "@mui/material";
 
 export function Heading2({ children }) {
   return (
-    <Typography variant="h2" fontSize={{ xs: 28, sm: 34 }} fontWeight={400}>
+    <Typography variant="h2" lineHeight={1.5} fontSize={{ xs: 28, sm: 34 }} fontWeight={400}>
       {children}
     </Typography>
   );
